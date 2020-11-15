cf232b89cb7e8c858f6b1fd39836ee12a495b129
tntttom <tntt_tom@me.com>
2020-11-15 14:21:02 -0600

    updated route paths

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,9 +1,9 @@
 const ProductController = require("../controllers/product.controller");
 
 module.exports = function (app) {
-  app.get("/products", ProductController.getProducts);
-  app.get("/product/:id", ProductController.getProduct);
-  app.post("/products/new", ProductController.createProduct);
-  app.put("/product/:id", ProductController.updateProduct);
-  app.delete("/product/:id", ProductController.deleteProduct);
+  app.get("/api/products", ProductController.getProducts);
+  app.get("/api/product/:id", ProductController.getProduct);
+  app.post("/api/products/new", ProductController.createProduct);
+  app.put("/api/product/:id", ProductController.updateProduct);
+  app.delete("/api/product/:id", ProductController.deleteProduct);
 };
